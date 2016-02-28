bee9541b768d7df0d8a394badedff0a2c9ab1d28
Rafa C <rafasantcont@yahoo.com.br>
2016-02-28 17:51:48 -0300

    Week 3: Refactory Homework 3.1.

--- Week 3 - Schema Design/hw3-1.py
+++ Week 3 - Schema Design/hw3-1.py
@@ -4,20 +4,14 @@ connection_string = "mongodb://localhost"
 connection = pymongo.MongoClient(connection_string)
 db = connection.school
 
-collection = db.students
+students = db.students
+cursor = students.aggregate([ { '$unwind' : '$scores' },
+    { '$match' : { 'scores.type' : 'homework' } },
+    { '$sort' : { '_id' : pymongo.ASCENDING, 'scores.score': pymongo.ASCENDING } }
+])
 
-for student in collection.find():
-    scores = student["scores"]
-
-    if scores[2]["type"] != "homework" or scores[3]["type"] != "homework":
-        print ("ERRO")
-        break
-
-    if scores[2]["score"] < scores[3]["score"]:
-        scores.pop(2)
-    else:
-        scores.pop(3)
-
-    collection.update_one({'_id':student["_id"]}, {'$set': {'scores':scores}})
-
-    print(scores)
+id = ''
+for doc in cursor:
+    if doc['_id'] != id:
+        id = doc['_id']
+        students.update({ '_id': doc['_id']}, { '$pull': { 'scores': { 'score': doc['scores']['score'] } } } )
